349fd93bba88e7c91bf14e01fe1885db0aef22d1
nitta manami <mamuchydawayo.0714@icloud.com>
2026-06-28 02:12:02 +0900

    ユーザー登録機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def after_sign_up_path_for(resource)
+    destroy_user_session_path
+  end
 end
