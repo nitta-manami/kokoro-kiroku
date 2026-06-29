d9bc910c4296d866fe9cd0b7a09c531105452713
nitta manami <mamuchydawayo.0714@icloud.com>
2026-06-29 20:01:32 +0900

    体調記録作成機能実装

---
+++ app/controllers/health_records_controller.rb
@@ -0,0 +1,23 @@
+class HealthRecordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @health_record = HealthRecord.new
+  end
+
+  def create
+    @health_record = current_user.health_records.new(health_record_params)
+
+    if @health_record.save
+      redirect_to home_path, notice: "体調を記録しました🌸"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def health_record_params
+    params.require(:health_record).permit(:content)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :medication_records, dependent: :destroy
+  has_many :health_records, dependent: :destroy
 end
