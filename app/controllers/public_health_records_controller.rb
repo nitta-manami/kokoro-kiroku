925cd2ed1e0fce95a412e034516d27207e5b7b4a
nitta manami <mamuchydawayo.0714@icloud.com>
2026-06-30 04:48:57 +0900

    みんなの記録一覧実装

---
+++ app/controllers/public_health_records_controller.rb
@@ -0,0 +1,8 @@
+class PublicHealthRecordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @health_records = HealthRecord.includes(:user)
+                                  .order(created_at: :desc)
+  end
+end
