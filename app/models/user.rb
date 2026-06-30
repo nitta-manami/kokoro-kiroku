c70d6862f42ad3f5b91abe7241012b68702eadac
nitta manami <mamuchydawayo.0714@icloud.com>
2026-07-01 02:56:15 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 20 }
+
   has_many :medication_records, dependent: :destroy
   has_many :health_records, dependent: :destroy
   has_many :likes, dependent: :destroy
